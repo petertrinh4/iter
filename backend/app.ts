c6bf14a5c7301d57dd441c5f5bc37ef3ebf18f31
Peter Trinh <petertrinh04@gmail.com>
2026-07-03 02:31:47 -0400

    Hot fix: register api

--- backend/app.ts
+++ backend/app.ts
@@ -1,23 +1,12 @@
 import express from "express";
 import cors from "cors";
-
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://www.teamtitan.net"
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-
-app.get("/", (_, res) => {
-  res.send("🏃‍♂️ Running App API is up and running!");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
--- backend/lambda.ts
+++ backend/lambda.ts
@@ -1,14 +1,7 @@
-import serverless from 'serverless-http';
-import app from './app.js';
-import { connectDB } from './utils/db.js';
+import serverless from "serverless-http";
+import app from "./app.js";
+import { connectDB } from "./utils/db.js";
 
-const expressHandler = serverless(app);
+await connectDB();
 
-export const handler = async (
-  event: any,
-  context: any
-) => {
-  await connectDB();
-
-  return expressHandler(event, context);
-};
+export const handler = serverless(app);
